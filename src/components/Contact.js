3323f987af06dcffda1b794adda8be34bc3dafc2
chibuikeA11 <chibuikecharles111@gmail.com>
2023-11-23 16:31:40 +0000

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,8 +37,8 @@ const Contact = () => {
           <p className='text-4xl font-bold inline border-b-4 border-blue-300 text-gray-300'>Contact</p>
           <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email </p>
         </div>
-        <input className='bg-[#ccd6f6] p-2' type='text' placeholder='Name' name='name' />
-        <input className='my-4 p-2 bg-[#ccd6f6]' type='email' placeholder='Email' name='email' required />
+        <input className='bg-[#ccd6f6] p-2' type='user_name' placeholder='Name' name='user_name' />
+        <input className='my-4 p-2 bg-[#ccd6f6]' type='email-address' placeholder='Email' name='email-address' required />
         <textarea className='bg-[#ccd6f6] p-2' name='message' rows='10' placeholder='Message'></textarea>
         <button className='text-white border-2 hover:bg-blue-300 hover:border-blue-300 px-4 py-3 my-8 mx-auto flex items-center'>
           Let's Collaborate
