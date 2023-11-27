8797aee91df76e5bd95afe00e25a75183f4467e1
chibuikeA11 <chibuikecharles111@gmail.com>
2023-11-27 09:10:44 +0000

    commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,7 +6,7 @@ import screenshot from '../assets/screenshot.png'
 
 function Work() {
   return (
-    <div className='w-full md:h-screen text-gray-300 bg-slate-950'>
+    <div className='w-full h-full min-h-screen text-gray-300 bg-slate-950'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div className='pb-8'>
             <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-blue-300'>Work</p>
